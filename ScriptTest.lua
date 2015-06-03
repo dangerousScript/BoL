4457647cb1d1e895dae5244299c2c252d33b7af2
Stefan <janjicstefan163@gmail.com>
2015-06-03 19:11:00 +0200

    testing
    blablabla

---
+++ ScriptTest.lua
@@ -0,0 +1,10 @@
+--[[
+        This is my frist script 
+        But who cares about fucking comments :O
+
+  YOLOOOOOOOOOOO
+]]
+
+if myHero.charName ~= "Ashe" then return end
+
+local version = 0.10
