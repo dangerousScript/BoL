cd2a6bc9487f3bf5c7bb541c8dcf9668658e28ad
Stefan <janjicstefan163@gmail.com>
2015-06-03 19:16:04 +0200

    Update ScriptTest.lua

--- ScriptTest.lua
+++ ScriptTest.lua
@@ -1,8 +1,11 @@
 --[[
         This is my frist script 
         But who cares about fucking comments :O
-
+        
   YOLOOOOOOOOOOO
+
+Changelogs:
+        0.10 - Added 13 lines of code !!!!!!! FUCK YEAH!
 ]]
 
 if myHero.charName ~= "Ashe" then return end
