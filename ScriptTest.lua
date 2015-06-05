d8052dee9d411ce154b462222cd798d1b61cf281
Stefan <janjicstefan163@gmail.com>
2015-06-05 16:42:35 +0200

    Bug or what

--- ScriptTest.lua
+++ ScriptTest.lua
@@ -6,12 +6,21 @@
 
 Changelogs:
         0.10 - Added 13 lines of code !!!!!!! FUCK YEAH!
-]]
+        0.11 - I WANNA LEARN TO SCRIPT FOR BoL 1 & BoL 2 !!!!
+        
+ TODO:
+        Add drawings
+        Spell cast
+        Menu
+        Something more when i learn
+]]--
 
 if myHero.charName ~= "Ashe" then return end
 
 local version = 0.10
 local AUTOUPDATE = true
+
+
 
 
 
