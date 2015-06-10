6e46dd6f708e079ef96e3aa8e25ecd727a355273
Stefan <janjicstefan163@gmail.com>
2015-06-10 20:27:02 +0200

    Draw text

--- ScriptTest.lua
+++ ScriptTest.lua
@@ -7,15 +7,15 @@
 Changelogs:
         0.10 - Added 13 lines of code !!!!!!! FUCK YEAH!
         0.11 - I WANNA LEARN TO SCRIPT FOR BoL 1 & BoL 2 !!!!
+        0.12 - Drawings added 
         
  TODO:
-        Add drawings
         Spell cast
         Menu
         Something more when i learn
 ]]--
 
-local version = 0.10
+local version = 0.12
 local AUTOUPDATE = true
 
 -- Get our champion
@@ -28,8 +28,9 @@ function OnTick()
 end
 
 function OnDraw()
-        if (myHero.mana < 200) then
-                DrawText("text", TextSize, X, Y, HexColor)
+        if (myHero.mana < 150) then
+                -- DrawText("XXX", TextSize, X, Y, HexColor)
+                DrawText("Warning: LOW MANA! Drink blue potion!", 18, 100, 100, 0x0000FF)
         end
 end
 
