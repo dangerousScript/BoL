e2c4bd42c681da91c1c0db7753e52912af496512
Stefan <janjicstefan163@gmail.com>
2015-06-11 02:03:08 +0200

    Fixed

--- ScriptTest.lua
+++ ScriptTest.lua
@@ -19,19 +19,25 @@ Changelogs:
 local version = 0.13
 local AUTOUPDATE = true
 
+-- / Auto-Update Function / --
+local script_downloadName = "Katarina - The Sinister Blade"
+local script_downloadHost = "raw.github.com"
+local script_downloadPath = "/UglyOldGuy/BoL/master/Katarina%20-%20The%20Sinister%20Blade.lua" .. "?rand=" .. math.random(1, 10000)
+local script_downloadUrl = "https://" .. script_downloadHost .. script_downloadPath
+local script_filePath = SCRIPT_PATH .. GetCurrentEnv().FILE_NAME
+-- get username
 user = GetUser()
-
+-- Get our champion
+myHero = GetMyHero()
 function OnLoad()
         -- PrintChat("<font color='#0000FF'> >> Kha'zix - The Voidreaver 1.2.3 Loaded!! <<</font>")
         PrintChat("<font color='#FF0033'> Welcome: </font> " .. "<font color='#00CC00'>" .. user .. "</font>")
+        PrintChat("<font color='#FF9900'> You are playing: </font>" .. "<font color='#CC3399'" .. myHero .. "</font>")
 end
-
--- Get our champion
-myHero = GetMyHero()
 
 function OnTick()
         if (myHero.health < 200) then
-                PrintChat("Warning: LOW HP! Drink a potion!"
+                PrintChat("Warning: LOW HP! Drink a potion!")
         end
 end
 
