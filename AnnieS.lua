c6f1a6388642153741fb5aad4a88028b2d7a05d1
Stefan <janjicstefan163@gmail.com>
2015-10-08 21:00:43 +0200

    Testing autoupdate

--- AnnieS.lua
+++ AnnieS.lua
@@ -1,3 +1,4 @@
+local Version = "0.12"
 --[[
 	Changelogs:
 		-0.10:
@@ -10,62 +11,74 @@
 			Add Autoupdate function
 ]]
 
---[[  AUTOUPDATE 
-	
-local autoupdate = true
-function Update(arg)
-	if arg ~= "force" then
-		if not autoupdate then
-			p("Autoupdate's disabled")
-		return
-		end
-	end
-		p("Updating S1mple_Ziggs")
-		local ServerData = GetWebResult(Update_HOST, "/Scarjit/Scripts/master/S1mple_Ziggs.version")
-		if ServerData then
-			ServerVersion = type(tonumber(ServerData)) == "number" and tonumber(ServerData) or nil
-			if ServerVersion then
-				if tonumber(version) < ServerVersion then
-					p("Update found")
-					p("Local Version: "..version" <==> ServerVersion: "..ServerVersion)
-					p("Updating, don't press F9")
-					DelayAction(function() DownloadFile(Update_URL, Update_FILE_PATH, function () p("Successfully updated. ("..version.." => "..ServerVersion.."), press F9 twice to load the updated version.") end) end, 3)
-				elseif  tonumber(version) == ServerVersion then
-					p("No Update found")
-				elseif tonumber(version) > ServerVersion then
-					p("WARNING: There is something wrong, with the Updater")
-					p("or you have manually changed your Version Number")
-				end
+-- Hero name check
+if myHero.charName ~= "Annie" then return end
+
+
+
+_G.Annie_Autoupdate = true
+
+
+-- / Auto-Update Function / --
+local script_downloadName = "AnnieS"
+local script_downloadHost = "raw.github.com"
+local script_downloadPath = "/janja96/BoL/blob/master/AnnieS.lua" .. "?rand=" .. math.random(1, 10000)
+local script_downloadUrl = "https://" .. script_downloadHost .. script_downloadPath
+local script_filePath = SCRIPT_PATH .. GetCurrentEnv().FILE_NAME
+
+
+function script_Messager(msg) print("<font color=\"#FF0000\">" .. script_downloadName .. ":</font> <font color=\"#FFFFFF\">" .. msg .. ".</font>") end
+
+if _G.Annie_Autoupdate then
+	local script_webResult = GetWebResult(script_downloadHost, script_downloadPath)
+	if script_webResult then
+		local script_serverVersion = string.match(script_webResult, "local%s+version%s+=%s+\"%d+.%d+\"")
+		
+		if script_serverVersion then
+			script_serverVersion = tonumber(string.match(script_serverVersion or "", "%d+%.?%d*"))
+
+			if not script_serverVersion then
+				script_Messager("Please contact the developer of the script \"" .. script_downloadName .. "\", since the auto updater returned an invalid version.")
+				return
 			end
-		else
-			p("Autoupdate failed")
+
+			if tonumber(Version) < script_serverVersion then
+				script_Messager("New version available: " .. script_serverVersion)
+				script_Messager("Updating, please don't press F9")
+				DelayAction(function () DownloadFile(script_downloadUrl, script_filePath, function() script_Messager("Successfully updated the script, please reload!") end) end, 2)
+			else
+				script_Messager("You've got the latest version: " .. script_serverVersion)
+			end
 		end
+	else
+		script_Messager("Error downloading server version!")
+	end
 end
-
-]]
-
+-- / Auto-Update Function / --
 
 
+local ts 
 
-
--- We will store the target selector on this variable
-local ts
-local Version = 0.10
- 
 -- Execute only at start of the game
 function OnLoad()
 	-- Menu
-	Config = scriptConfig("Annie - TheDaddy", "AnnieSave")
-	Config:addParam("drawCircle", "Draw Circle", SCRIPT_PARAM_ONOFF, true)
-	Config:addParam("printHp", "Print Health warning", SCRIPT_PARAM_ONKEYTOGGLE, false, string.byte("A"))
-	Config:addParam("combo", "Combo mode", SCRIPT_PARAM_ONKEYDOWN, false, 32)
+	Config = scriptConfig("AnnieS", "AnnieSave")
+	-- draw menu
+	Config:addSubMenu("["..myHero.charName.."] - Draw", "draw")
+		Config.draw:addParam("drawCircle", "Draw Circle", SCRIPT_PARAM_ONOFF, true)
+		Config.draw:addParam("printHp", "Print Health warning", SCRIPT_PARAM_ONKEYTOGGLE, false, string.byte("A"))
+	-- combo menu
+	Config:addSubMenu("["..myHero.charName.."] - Combo", "combo")
+		Config.combo:addParam("combo", "Combo mode", SCRIPT_PARAM_ONKEYDOWN, false, 32)
+
+	-- menu version
+	Config:addParam("version", "Version: " .. Version, SCRIPT_PARAM_ONOFF, true)
  
  
 	-- We create a target selector
-	ts = TargetSelector(TARGET_LOW_HP_PRIORITY,650)
-
+	ts = TargetSelector(TARGET_LOW_HP_PRIORITY,630)
 	-- Welcome message
-	PrintChat("<font color=\"#FF0000\">[Annie]:</font> <font color=\"#FFFFFF\">Loaded Version: </font>" ..Version.. "<font color=\"#FFFFFF\"> by stefisa</font>")
+	PrintChat("<font color=\"#FF0000\">[AnnieS]:</font> <font color=\"#FFFFFF\">Loaded Version: </font>" ..Version.. "<font color=\"#FFFFFF\"> by stefisa</font>")
 end
  
 -- Execute 10 times per second
@@ -108,7 +121,7 @@ end
 function OnDraw()
 	--Draw circles only if activated on menu
 	if (Config.drawCircle) then
-		DrawCircle(myHero.x, myHero.y, myHero.z, 650, 0x111111)
+		DrawCircle(myHero.x, myHero.y, myHero.z, 630, 0x111111)
 	end
 	
 	-- Show HP warning
