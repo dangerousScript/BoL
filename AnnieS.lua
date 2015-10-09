206b6dd95d2c929bb1bafba53405c9d072ca852d
Stefan <janjicstefan163@gmail.com>
2015-10-09 03:11:26 +0200

    AutoUpdater fix

--- AnnieS.lua
+++ AnnieS.lua
@@ -1,4 +1,4 @@
-local Version = "0.24"
+local Version = "0.25"
 --[[
 	Changelogs:
 		-0.10:
@@ -14,51 +14,8 @@ local Version = "0.24"
 -- Hero name check
 if myHero.charName ~= "Annie" then return end
 
-
---[[
-_G.Annie_Autoupdate = true
-
-
--- / Auto-Update Function / --
-local script_downloadName = "AnnieS"
-local script_downloadHost = "raw.github.com"
-local script_downloadPath = "/janja96/BoL/master/AnnieS.lua" .. "?rand=" .. math.random(1, 10000)
-local script_downloadUrl = "https://" .. script_downloadHost .. script_downloadPath
-local script_filePath = SCRIPT_PATH .. GetCurrentEnv().FILE_NAME
-
-
-function script_Messager(msg) print("<font color=\"#FF0000\">" .. script_downloadName .. ":</font> <font color=\"#FFFFFF\">" .. msg .. ".</font>") end
-
-if _G.Annie_Autoupdate then
-	local script_webResult = GetWebResult(script_downloadHost, script_downloadPath)
-	if script_webResult then
-		local script_serverVersion = string.match(script_webResult, "local%s+version%s+=%s+\"%d+.%d+\"")
-		
-		if script_serverVersion then
-			script_serverVersion = tonumber(string.match(script_serverVersion or "", "%d+%.?%d*"))
-
-			if not script_serverVersion then
-				script_Messager("Please contact the developer of the script \"" .. script_downloadName .. "\", since the auto updater returned an invalid version.")
-				return
-			end
-
-			if tonumber(Version) < script_serverVersion then
-				script_Messager("New version available: " .. script_serverVersion)
-				script_Messager("Updating, please don't press F9")
-				DelayAction(function () DownloadFile(script_downloadUrl, script_filePath, function() script_Messager("Successfully updated the script, please reload!") end) end, 2)
-			else
-				script_Messager("You've got the latest version: " .. script_serverVersion)
-			end
-		end
-	else
-		script_Messager("Error downloading server version!")
-	end
-end
--- / Auto-Update Function / --
-]]
-
 -- / Auto Update Function / --
-local sVersion = '0.24';
+local sVersion = '0.25';
 local rVersion = GetWebResult('raw.githubusercontent.com', '/janja96/BoL/master/Versions/AnnieS.version?no-cache=' .. math.random(1, 25000));
 
 if ((rVersion) and (tonumber(rVersion) ~= nil)) then
