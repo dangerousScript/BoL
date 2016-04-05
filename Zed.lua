ceed6d1be08580dae899bbeb41ec9a1ecad5cae2
Stefan <janjicstefan163@gmail.com>
2016-04-05 03:48:12 +0200

    Update Zed.lua

--- Zed.lua
+++ Zed.lua
@@ -7,7 +7,7 @@ if myHero.charName ~= "Brand" then return end  -- promeni u Zed
 local AutoUpdate = true
 
 -- promenljive za update --
-local scriptVersion = 0.11
+local scriptVersion = 0.10
 local StridePage = 'raw.githubusercontent.com'
 local ScriptLink = '/janja96/BoL/blob/master/Zed.lua'
 local VresionLink = '/janja96/BoL/master/Versions/Zed.version?no-cache=' .. math.random(1, 25000)
@@ -49,48 +49,48 @@ class 'Update'
 function Update:__init()
   if not AutoUpdate then return end
   self.Version = scriptVersion
-  self.DownloadPath = 'https://'..StridePage..ScriptLink
+  self.DownloadPath = 'http://'..StridePage..ScriptLink
   self.SavePath = SCRIPT_PATH .. _ENV.FILE_NAME
   if os.clock() < 180 then
-    self:Check()
+	self:Check()
   else
-    DelayAction(function()
-      printChat('The game is already in progress. Disabling auto update.')
-    end, 2)
+		DelayAction(function()
+			printChat('The game is already in progress. Disabling auto update.')
+		end, 2)
   end
 end
 
 function Update:RequireUpdate()
   self.NewVersion = GetWebResult(StridePage, VersionLink)
   if self.NewVersion then
-    self.NewVersion = string.match(self.NewVersion, '%d.%d%d')
-    self.NewVersion = tonumber(self.NewVersion)
-    if self.NewVersion and self.NewVersion > scriptVersion then
-      DelayAction(function()
-  		printChat('New version v'..self.NewVersion..' found! Downloading... Do not press F9!')
-  	  end, 2)
-  	  return true
-    else
-      DelayAction(function()
-  		printChat('No new updates found.')
-  	  end, 2)
-  	  return false
-    end
+	self.NewVersion = string.match(self.NewVersion, '%d.%d%d')
+		self.NewVersion = tonumber(self.NewVersion)
+	if self.NewVersion and self.NewVersion > scriptVersion then
+	  DelayAction(function()
+		printChat('New version v'..self.NewVersion..' found! Downloading... Do not press F9!')
+	  end, 2)
+	  return true
+	else
+	  DelayAction(function()
+		printChat('No new updates found.')
+	  end, 2)
+	  return false
+	end
   end
 end
 
 function Update:Check()
-  if self.RequireUpdate() then
-    DownloadFile(self.DownloadPath, self.SavePath,
-    function()
-        if FileExist(self.SavePath) then
-          DelayAction(function()
-            printChat('Script updated! Please reload BOL for changes to take effect!')
-          end, 3)
-        end
-        end)
-      else
-  end
+  if self:RequireUpdate() then
+	DownloadFile(self.DownloadPath, self.SavePath,
+	function()
+	  if FileExist(self.SavePath) then
+		DelayAction(function()
+		  printChat('Script updated! Please reload BOL for changes to take effect!')
+		end, 3)
+	  end
+	end)
+  else
+	end
 end
 
 --[[
